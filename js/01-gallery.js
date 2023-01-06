5bc17ec84ca8ba4edae28f79d47e140aa5a44d17
EugeneKom <EugeneKomissarenko@gmail.com>
2023-01-06 23:45:36 +0100

    add modal markup and eventLis
    dynamically adds markup, add event listener, add modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,75 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// Create marcup
+
+const wrapperGallery = document.querySelector('.gallery')
+
+const marcup = galleryItems.map(({original,description}) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src=${original}
+            data-source=${original}
+            alt=${description}
+            />
+        </a>
+    </div>`).join('')
+
+wrapperGallery.insertAdjacentHTML('afterbegin',marcup)
+
+// --------------------------------------
+
+
+// Remove default link behavior   (need optimisation)
+
+const imgLinkEl = document.querySelectorAll('.gallery__link');
+
+imgLinkEl.forEach(link => {
+    link.addEventListener('click', evt => evt.preventDefault())
+})
+
+// ---------------------------------------
+
+// Library LightBox
+
+wrapperGallery.addEventListener('click', onClickModalOpen)
+
+function onClickModalOpen (evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+    const imgEl = evt.target
+
+    if (imgEl) {
+        createModal(imgEl)
+    }
+    
+}
+
+
+function createModal(imgEl) {
+    const modalEl = basicLightbox.create(`<div class="modal"><img
+        src=${imgEl.src}
+        alt=${imgEl.alt}
+        /></div>`, {closable: true});
+        modalEl.show()
+}
+
+
+
+//  Тут я здался  (дублируется слушатель события)
+
+// function onClickCloseModal (evt, modalEl) {
+//     if (evt.code === 'Escape') {
+//         modalEl.close()
+//         wrapperGallery.removeEventListener('keydown', onClickCloseModal)
+//     }
+// }
+// ------------------------------------
+// if (modalEl.visible()) {
+//     wrapperGallery.addEventListener('keydown' ,(e) => onClickCloseModal(e, modalEl))
+//     }
+
