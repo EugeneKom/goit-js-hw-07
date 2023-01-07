8e551d9f6a75a35c7c97bf1f8c2ec90239f02d88
EugeneKom <EugeneKomissarenko@gmail.com>
2023-01-07 16:37:41 +0100

    add liberty
    add liberty SimpleLightbox(cdn) , add delay and captionsData

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+//  Create marup ------------
+const wrapperGallery = document.querySelector('.gallery')
+
+const marcup = galleryItems.map(({original,description, preview}) => 
+    `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`).join('')
+
+wrapperGallery.insertAdjacentHTML('afterbegin',marcup)
+
+
+new SimpleLightbox('.gallery a', {'captionsData': 'alt', 'captionDelay': 250});
+
+
+
